fc476ab548209a51015a87159314480f8db40c04
Gabriel Gonçalves <gabriel.gopp@gmail.com>
2025-04-11 15:36:52 -0300

    refactor(docs): adjust layout structure and add Navbar for better navigation
    The layout structure was modified to include a Navbar component for improved navigation. The main container's padding was adjusted to accommodate the new Navbar, enhancing the overall user experience.

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,19 +1,23 @@
 "use client"
 
 import { DocsSidebar } from "@/components/DocsSidebar";
+import Navbar from "@/components/Navbar";
 
 
 export default function DocsLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  return (
-    <div className="flex min-h-screen text-white container mx-auto px-8 md:px-14">
-      <DocsSidebar />
-      <main className="flex-1 p-8 overflow-y-auto">
-        {children}
-      </main>
-    </div>
-  );
+    return (
+        <div className="pt-14">
+            <Navbar />
+            <div className="flex min-h text-white container mx-auto px-8 md:px-14">
+                <DocsSidebar />
+                <main className="flex-1 p-8 overflow-y-auto">
+                    {children}
+                </main>
+            </div>
+        </div>
+    );
 }
